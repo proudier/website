de37bd25adb5b524bccb0ca7e85450036e8306e4
Pierre ROUDIER <pierre@roudier.io>
2018-03-06 10:40:11 +0100

    Publish scrip now build site to make sure sitemap.xml has proper URL

--- publish.sh
+++ publish.sh
@@ -2,6 +2,9 @@
 
 . ~/.ovh
 
+bundle exec jekyll clean
+bundle exec jekyll build
+
 lftp -u ${USER},${PASSWD} -e "mirror --verbose --parallel=4 --delete --reverse _site/ www; quit" ${URL}
 if [ $? -eq 0 ]; then
 	logger $0 - Push to OVH: success
